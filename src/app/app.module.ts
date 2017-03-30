d23fd87f13a284986e365b05f526de1f7f6db0ea
Unknown <mauritsbastiaans@hotmail.com>
2017-03-30 21:18:55 +0200

    Framework additional setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,33 +2,31 @@
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { Cards } from '../pages/cards/cards';
-import { Checklist } from '../pages/checklist/checklist';
+import { CardsPage } from '../pages/cards/cards';
+import { ChecklistPage } from '../pages/checklist/checklist';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
-    declarations: [
-        MyApp,
-        HomePage,
-        Cards,
-        Checklist
-    ],
-    imports: [
-        IonicModule.forRoot(MyApp)
-    ],
-    bootstrap: [IonicApp],
-    entryComponents: [
-        MyApp,
-        HomePage,
-        Cards,
-        Checklist
-    ],
-    providers: [
-        StatusBar,
-        SplashScreen,
-        { provide: ErrorHandler, useClass: IonicErrorHandler }
-    ]
+  declarations: [
+    MyApp,
+    HomePage,
+    CardsPage,
+    ChecklistPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
 export class AppModule { }
--- src/pages/cards/cards.ts
+++ src/pages/cards/cards.ts
@@ -1,19 +1,15 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
-import { Checklist} from '../checklist/checklist'
-
 @Component({
-    selector: 'cards',
-    templateUrl: 'cards.html'
+  selector: 'page-cards',
+  templateUrl: 'cards.html'
 })
-export class Cards
-{
+export class CardsPage {
 
-    constructor(public navCtrl: NavController)
-    {
-        this.navCtrl.push(Checklist);
-    }
+  constructor(public navCtrl: NavController) {
+  
+  }
 
 }
